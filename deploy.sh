ea48e21917d8a91a556513a7b016a32f1027641f
Antoine <antoine.zanardi@epitech.eu>
2018-05-14 23:42:22 +0200

    fix(Travis): Deploy working ?

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,4 @@ cp deploy_key.pem ~/.ssh/deploy_key.pem
 chmod 600 ~/.ssh/deploy_key.pem
 ssh-add ~/.ssh/deploy_key.pem
 git remote add deploy ssh://root@vps514431.ovh.net/home/git/antoinezanardi.git
-git push deploy +master:refs/head/master
 git push deploy
