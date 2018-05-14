718db80e098b3534f649992675d724573f89732e
Antoine <antoine.zanardi@epitech.eu>
2018-05-14 20:36:02 +0200

    fix(Travis): deploy script should be good

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 eval "$(ssh-agent -s)"
 mkdir ~/.ssh
-mv .travis-config ~/.ssh/config
-mv .deploy.pem ~/.ssh/deploy_key.pem
+mv .deploy_key.pem ~/.ssh/deploy_key.pem
 chmod 600 ~/.ssh/deploy_key.pem
 git remote add deploy ssh://vps514431.ovh.net/home/git/antoinezanardi.git
 git push deploy master
