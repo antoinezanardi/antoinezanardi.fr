3b8761ceb1f8b7b2b10ae700902e575bb29741f7
Antoine <antoine.zanardi@epitech.eu>
2018-05-14 20:36:40 +0200

    fix(Travis): deploy script should be good

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 eval "$(ssh-agent -s)"
 mkdir ~/.ssh
-mv .deploy_key.pem ~/.ssh/deploy_key.pem
+mv deploy_key.pem ~/.ssh/deploy_key.pem
 chmod 600 ~/.ssh/deploy_key.pem
 git remote add deploy ssh://vps514431.ovh.net/home/git/antoinezanardi.git
 git push deploy master
