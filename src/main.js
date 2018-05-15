c218f9ca322d855f13b5754ab82904f5b1fb4285
Antoine <antoine.zanardi@epitech.eu>
2018-05-15 15:50:59 +0200

    feat(FontAwesome): FA installed locally

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import App from './App'
 import router from './router'
 import "bootstrap";
 import "aos";
+import "@fortawesome/fontawesome-free-webfonts/css/fontawesome.css";
+import "@fortawesome/fontawesome-free-webfonts/css/fa-brands.css";
+import "@fortawesome/fontawesome-free-webfonts/css/fa-regular.css";
+import "@fortawesome/fontawesome-free-webfonts/css/fa-solid.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "aos/dist/aos.css"
 import "@/assets/css/main.css";
