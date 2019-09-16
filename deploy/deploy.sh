ca23819c6e751c970a918f235c0c9395060d613c
antoinezanardi <antoine.zanardi@epitech.eu>
2019-09-16 11:49:41 +0200

    feat(Portfolio): V2 is out

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 eval "$(ssh-agent -s)"
 if [ ! -d ~/.ssh ]; then mkdir ~/.ssh; fi
-cp deploy_key.pem ~/.ssh/deploy_key.pem
+cp ./deploy/deploy_key.pem ~/.ssh/deploy_key.pem
 chmod 600 ~/.ssh/deploy_key.pem
 ssh-add ~/.ssh/deploy_key.pem
 git remote add deploy ssh://root@vps514431.ovh.net/home/git/antoinezanardi.fr.git
