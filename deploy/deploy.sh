05f1391f722c14c4af9bddfad80e85d1d6700b63
antoinezanardi <antoine.zanardi@epitech.eu>
2021-03-15 23:23:45 +0100

    ci(Server): New deploy key

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 eval "$(ssh-agent -s)"
 if [ ! -d ~/.ssh ]; then mkdir ~/.ssh; fi
-cp ./deploy/deploy_key.pem ~/.ssh/deploy_key.pem
-chmod 600 ~/.ssh/deploy_key.pem
-ssh-add ~/.ssh/deploy_key.pem
+cp ./deploy/deploy-key ~/.ssh/deploy-key
+chmod 600 ~/.ssh/deploy-key
+ssh-add ~/.ssh/deploy-key
 git remote add deploy ssh://debian@vps514431.ovh.net/home/debian/antoinezanardi.fr.git
 git push deploy master
