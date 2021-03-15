a2af166c2e5dded1736f040b65575010f4cce2a4
antoinezanardi <antoine.zanardi@epitech.eu>
2021-03-16 00:31:33 +0100

    ci(Server): New deploy key

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,4 +5,5 @@ cp ./deploy/deploy-key ~/.ssh/deploy-key
 chmod 600 ~/.ssh/deploy-key
 ssh-add ~/.ssh/deploy-key
 git remote add deploy ssh://debian@vps-841374ca.vps.ovh.net/home/debian/antoinezanardi.fr.git
+git fetch --depth=1000000
 git push deploy master
