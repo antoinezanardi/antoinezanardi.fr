e197d21cb514686761def37b174f4bf9c3bfa510
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2025-02-04 23:01:16 +0100

    test: improve tests coverage

--- app/App.vue
+++ app/App.vue
@@ -26,13 +26,13 @@ import aos from "aos";
 import { AboutMe, MyProfile, MySkills, MyPortfolio, MyExperience, MyEducation, PageFooter, NavBar } from "#components";
 import { onMounted, useNuxtApp, useHead } from "#imports";
 
-const { t, locale } = useI18n();
+const { t } = useI18n();
 const { $bootstrap } = useNuxtApp();
 
 useHead({
   title: "Antoine ZANARDI",
   meta: [{ name: "description", content: t("App.meta.description") }],
-  htmlAttrs: { lang: locale.value },
+  htmlAttrs: { lang: "fr" },
 });
 
 onMounted((): void => {
