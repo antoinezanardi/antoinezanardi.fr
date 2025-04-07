722207a51bbb847537a30fc7a754cfb9bf4eb154
Antoine ZANARDI <antoine.zanardi.partner@decathlon.com>
2025-04-07 13:30:23 +0200

    feat(eslint): update constants to TypeScript and add cucumber config

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,20 +4,20 @@ import { ESLINT_CUCUMBER_CONFIG } from "./config/eslint/flat-configs/eslint.cucu
 import { ESLINT_IGNORES } from "./config/eslint/eslint.constants";
 import { ESLINT_GLOBAL_CONFIG } from "./config/eslint/flat-configs/eslint.global-config";
 import { ESLINT_IMPORT_CONFIG } from "./config/eslint/flat-configs/eslint.import-config";
-import { ESLINT_TYPESCRIPT_CONFIG } from "./config/eslint/flat-configs/eslint.typescript-config.js";
+import { ESLINT_TYPESCRIPT_CONFIG } from "./config/eslint/flat-configs/eslint.typescript-config";
 import { ESLINT_DECLARATIONS_CONFIG } from "./config/eslint/flat-configs/eslint.declarations-config";
-import { ESLINT_TYPESCRIPT_DECLARATION_CONFIG } from "./config/eslint/flat-configs/eslint.typescript-declaration-config.js";
-import { ESLINT_VUE_CONFIG } from "./config/eslint/flat-configs/eslint.vue-config.js";
+import { ESLINT_TYPESCRIPT_DECLARATION_CONFIG } from "./config/eslint/flat-configs/eslint.typescript-declaration-config";
+import { ESLINT_VUE_CONFIG } from "./config/eslint/flat-configs/eslint.vue-config";
 import { ESLINT_COMPOSABLES_CONFIG } from "./config/eslint/flat-configs/eslint.composables-config";
-import { ESLINT_PAGES_CONFIG } from "./config/eslint/flat-configs/eslint.pages-config.js";
-import { ESLINT_STYLISTIC_CONFIG } from "./config/eslint/flat-configs/eslint.stylistic-config.js";
+import { ESLINT_PAGES_CONFIG } from "./config/eslint/flat-configs/eslint.pages-config";
+import { ESLINT_STYLISTIC_CONFIG } from "./config/eslint/flat-configs/eslint.stylistic-config";
 import { ESLINT_CONFIG_FILES_CONFIG } from "./config/eslint/flat-configs/eslint.config-files-config";
 import { ESLINT_CLASSES_CONFIG } from "./config/eslint/flat-configs/eslint.classes-config";
-import { ESLINT_MODULES_CONFIG } from "./config/eslint/flat-configs/eslint.modules-config.js";
-import { ESLINT_PLUGINS_CONFIG } from "./config/eslint/flat-configs/eslint.plugins-config.js";
-import { ESLINT_NUXT_CONFIG_FILE_CONFIG } from "./config/eslint/flat-configs/eslint.nuxt-config-file-config.js";
-import { ESLINT_SERVER_FILES_CONFIG } from "./config/eslint/flat-configs/eslint.server-files.config.js";
-import { ESLINT_TESTS_CONFIG } from "./config/eslint/flat-configs/eslint.tests-config.js";
+import { ESLINT_MODULES_CONFIG } from "./config/eslint/flat-configs/eslint.modules-config";
+import { ESLINT_PLUGINS_CONFIG } from "./config/eslint/flat-configs/eslint.plugins-config";
+import { ESLINT_NUXT_CONFIG_FILE_CONFIG } from "./config/eslint/flat-configs/eslint.nuxt-config-file-config";
+import { ESLINT_SERVER_FILES_CONFIG } from "./config/eslint/flat-configs/eslint.server-files.config";
+import { ESLINT_TESTS_CONFIG } from "./config/eslint/flat-configs/eslint.tests-config";
 
 export default [
   {
